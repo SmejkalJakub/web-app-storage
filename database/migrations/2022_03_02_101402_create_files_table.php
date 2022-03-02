ce04d827cd34c34bcde1077af2d5681554d5f22f
Jakub Smejkal <smejkal.jakub99@gmail.com>
2022-03-02 11:26:07 +0100

    Procfile for heroku added

---
+++ database/migrations/2022_03_02_101402_create_files_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->primary('file_link');
+            $table->string('admin_link');
+            $table->string('file_storage_path');
+            $table->integer('downloads');
+            $table->date('delete_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
