8153253dfaef323e1314e34fe54177c36df5bd26
Jakub Smejkal <smejkal.jakub99@gmail.com>
2022-04-22 21:00:52 +0200

    Comments added to the author files

--- database/migrations/2022_03_02_101402_create_files_table.php
+++ database/migrations/2022_03_02_101402_create_files_table.php
@@ -4,6 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/*
+    Autorský soubor
+    Autor: Jakub Smejkal, Klára Formánková
+*/
+
 class CreateFilesTable extends Migration
 {
     /**
